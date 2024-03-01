89a0a399f7c516cd60624164055d4ed4a5e9d3af
Erick Jimenez <erick-rubi@hotmail.com>
2024-02-29 19:39:54 -0600

    Creacion de nuevo proyecto React-Context

---
+++ 01-react-form/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SimpleForm from './components/SimpleForm'
+import ReactHookForm from './components/ReactHookForm'
+
+function App() {
+  return (
+    <>
+      {/* <SimpleForm  /> */}
+      <ReactHookForm />
+    </>
+  )
+}
+
+export default App

e0fc23ccfc8bfdb9ecc62e3a6be2214e73c09cba
Erick Jimenez <erick-rubi@hotmail.com>
2024-03-05 21:09:42 -0600

    creación de header.scss

---
+++ 03-react-auth/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './components/Header/Header'
+import { Home, Dashboard, Login, Secret, Signup } from '@/pages'
+
+function App() {
+  return (
+    <>
+      <h1>React Auth</h1>
+      <Header />
+      <Home />
+      <Dashboard />
+      <Login />
+      <Secret />
+      <Signup />
+    </>
+  )
+}
+
+export default App

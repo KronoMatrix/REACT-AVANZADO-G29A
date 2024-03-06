3eee460ac8b2ee5f5ec0d8c16b9ebd180b8d1484
Erick Jimenez <erick-rubi@hotmail.com>
2024-03-05 21:38:41 -0600

    creacion de estilos y componentes

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header/Header'
 import { Home, Dashboard, Login, Secret, Signup } from '@/pages'
 
-function App() {
+function App () {
   return (
     <>
       <h1>React Auth</h1>
--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -2,7 +2,27 @@ import './header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='header'>
+      <a className='header-logo' href='/'>Logo</a>
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/home'>Home</a>
+        </li>
+        <li className='header__list-link'>
+          <a className='header__item-link' href='/dashboar'>Dashboard</a>
+        </li>
+        <li className='header__list-link'>
+          <a className='header__item-link' href='/login'>Login</a>
+        </li>
+        <li className='header__list-link'>
+          <a className='header__item-link' href='/secret'>Secret</a>
+        </li>
+        <li className='header__list-link'>
+          <a className='header__item-link' href='/signup'>Signup</a>
+        </li>
+      </ul>
+
+    </nav>
   )
 }
 

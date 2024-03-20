c6a9ddd0e3a18644ba00a95b66fa85a00d12b4d1
Erick Jimenez <erick-rubi@hotmail.com>
2024-03-19 20:28:04 -0600

    Logica en AuthContext

--- 03-react-auth/src/context/AuthContext.jsx
+++ 03-react-auth/src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react'
+import { jwtDecode } from 'jwt-decode'
 // CONTEXT tiene que ver con el manejo de estados globales en REACT.
 
 // Para usar Context, necesitamos seguir una serie de pasos.
@@ -11,10 +12,33 @@ const AuthContext = createContext()
 
 function AuthProvider ({ children }) {
   // Aquí va mi lógica
-  
+  const [isAuth, setIsAuth] = useState(false) // ¿Ésta autenticado?
+  const [userPayload, setUserPayload] = useState(null) // Información del Payload del token
+
+  const login = (token) => {
+    localStorage.setItem('token', token)
+    setIsAuth(true)
+    const payload = jwtDecode(token)
+    setUserPayload(payload)
+  }
+  const logout = () => {
+    localStorage.removeItem('token') // Remuevo el token del localStorage
+    setIsAuth(false)
+    setUserPayload(null)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token') // Obtengo el token del localStorage
+    if (token) { // si el token existe, entonces el usuario está autenticado.
+      const payload = jwtDecode(token)
+      setIsAuth(true)
+      setUserPayload(payload)
+    }
+  }, [])
+
   return (
   // El provider recibe una prop llamada value, que es la información que queremos compartir.
-    <AuthContext.Provider value={{  }}>
+    <AuthContext.Provider value={{ login, logout, isAuth, setIsAuth, userPayload }}>
       {children}
     </AuthContext.Provider>
   )

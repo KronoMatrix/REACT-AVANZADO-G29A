3c81c7d5e61a975270f4bb3347350a9e7d33c152
Erick Jimenez <erick-rubi@hotmail.com>
2024-03-23 14:39:54 -0600

    Dashboard perfil de usuario

--- 03-react-auth/src/pages/Dashboard.jsx
+++ 03-react-auth/src/pages/Dashboard.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getMeUserServices } from '../services/userServices'
 
 const Dashboard = () => {
+  const [userData, setUserData] = useState({})
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getMeUserServices(token)
+
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchUserData()
+  }, [token])
+
   return (
-    <div>Dashboard</div>
+    <>
+      <h1>Dashboard</h1>
+      {userData?.first_name && <p>Nombre: {userData.first_name}</p>}
+      {userData?.last_name && <p>Apelido: {userData.last_name}</p>}
+      {userData?.gender && <p>Género: {userData.gender}</p>}
+      {userData?.email && <p>E-mail: {userData.email}</p>}
+      {userData?.role && <p>Role: {userData.role}</p>}
+    </>
   )
 }
 
